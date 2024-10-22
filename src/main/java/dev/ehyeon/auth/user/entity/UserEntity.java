31e1db54750bdfac97e30f5a3ee0d7ecbd3e7306
49EHyeon42 <49ehyeon42@gmail.com>
2024-10-22 13:37:04 +0900

    feat: 기본 user entity, role 구현

---
+++ src/main/java/dev/ehyeon/auth/user/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package dev.ehyeon.auth.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public UserEntity(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ src/main/java/dev/ehyeon/auth/user/entity/UserRole.java
@@ -0,0 +1,5 @@
+package dev.ehyeon.auth.user.entity;
+
+public enum UserRole {
+    ADMIN, USER
+}
