611e023c56ca4fe9b4b8cfd92e56c7fdde1d88fb
49EHyeon42 <49ehyeon42@gmail.com>
2024-10-23 18:56:11 +0900

    feat: SecurityConfig 중 securityFilterChain 설정

---
+++ src/main/java/dev/ehyeon/auth/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package dev.ehyeon.auth.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // 필요 시 cors 설정
+                // sessionCreationPolicy default = SessionCreationPolicy.IF_REQUIRED
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers(HttpMethod.POST, "/api/v1/sign-in").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/sign-up").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/users/me").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .logout(httpSecurityLogoutConfigurer ->
+                        httpSecurityLogoutConfigurer
+                                .clearAuthentication(true)
+                                .invalidateHttpSession(true)
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/sign-out", HttpMethod.POST.name()))
+                                .permitAll()
+                                .deleteCookies("JSESSIONID")
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        response.setStatus(HttpServletResponse.SC_OK)))
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
