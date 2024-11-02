d4cc2158cf6056d73b9a04cdabf70d54f24235c3
49EHyeon42 <49ehyeon42@gmail.com>
2024-11-02 14:20:17 +0900

    fix: 불명확한 AntPathRequestMatcher() 대신 MvcRequestMatcher()로 변경

--- src/main/java/dev/ehyeon/auth/config/SecurityConfig.java
+++ src/main/java/dev/ehyeon/auth/config/SecurityConfig.java
@@ -16,13 +16,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final HandlerMappingIntrospector introspector;
     private final CustomUserDetailsService customUserDetailsService;
     private final ObjectMapper objectMapper;
 
@@ -35,9 +37,8 @@ public class SecurityConfig {
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
-                                .requestMatchers(HttpMethod.POST, "/api/v1/sign-in").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/api/v1/sign-up").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/v1/users/me").authenticated()
+                                .requestMatchers(mvcRequestMatcher(HttpMethod.POST, "/api/v1/sign-in")).permitAll()
+                                .requestMatchers(mvcRequestMatcher(HttpMethod.POST, "/api/v1/sign-up")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
@@ -45,7 +46,7 @@ public class SecurityConfig {
                         httpSecurityLogoutConfigurer
                                 .clearAuthentication(true)
                                 .invalidateHttpSession(true)
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/sign-out", HttpMethod.POST.name()))
+                                .logoutRequestMatcher(mvcRequestMatcher(HttpMethod.POST, "/api/v1/sign-out"))
                                 .permitAll()
                                 .deleteCookies("JSESSIONID")
                                 .logoutSuccessHandler((request, response, authentication) ->
@@ -55,6 +56,12 @@ public class SecurityConfig {
                 .addFilterBefore(new SessionAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
+    }
+
+    private MvcRequestMatcher mvcRequestMatcher(HttpMethod httpMethod, String pattern) {
+        MvcRequestMatcher mvcRequestMatcher = new MvcRequestMatcher(introspector, pattern);
+        mvcRequestMatcher.setMethod(httpMethod);
+        return mvcRequestMatcher;
     }
 
     @Bean
